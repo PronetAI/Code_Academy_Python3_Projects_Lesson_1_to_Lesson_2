ebbb3e6dbc15734e9400657953cd779b6c80b068
PronetAI <pronetaicorp@gmail.com>
2022-08-20 12:57:45 -0700

    Update lovely_loveseat_receipt.py

--- Hello World Lesson 1/lovely_loveseat_receipt.py
+++ Hello World Lesson 1/lovely_loveseat_receipt.py
@@ -1,12 +1,17 @@
+# All of the prices for each of the products
+lovely_loveseat_price,stylish_settee_price,luxurious_lamp_price=254.00,180.50,52.15
+
+# Descriptions for each for the products
 lovely_loveseat_description="Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
-lovely_loveseat_price=254.00
 stylish_settee_description="Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
-stylish_settee_price=180.50
 luxurious_lamp_description="Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
-luxurious_lamp_price=52.15
+
+# Initializing general variables
 sales_tax=0.088
 customer_one_total=0
 customer_one_itemization=""
+
+# Adding all of the items the user purchased
 customer_one_total+=lovely_loveseat_price
 customer_one_itemization+="\n"+lovely_loveseat_description
 customer_one_total+=stylish_settee_price
@@ -15,6 +20,9 @@ customer_one_total+=luxurious_lamp_price
 customer_one_itemization+="\n\n"+luxurious_lamp_description
 customer_one_tax=customer_one_total*sales_tax
 customer_one_total+=customer_one_tax
+
+
+# Printing out all the information
 print("Customer One Items:")
 print(customer_one_itemization + "\n")
 print("Customer One Total:")
