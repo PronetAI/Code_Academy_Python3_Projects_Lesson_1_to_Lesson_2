195dfe005357049beba79e9d5d58afb7700c223b
PronetAI <pronetaicorp@gmail.com>
2022-08-20 17:34:56 -0700

    Update space_weight.py

--- Control Flow Lesson 2/space_weight.py
+++ Control Flow Lesson 2/space_weight.py
@@ -1,21 +1,24 @@
-lovely_loveseat_description="Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
-lovely_loveseat_price=254.00
-stylish_settee_description="Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
-stylish_settee_price=180.50
-luxurious_lamp_description="Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
-luxurious_lamp_price=52.15
-sales_tax=0.088
-customer_one_total=0
-customer_one_itemization=""
-customer_one_total+=lovely_loveseat_price
-customer_one_itemization+="\n"+lovely_loveseat_description
-customer_one_total+=stylish_settee_price
-customer_one_itemization+="\n\n"+stylish_settee_description
-customer_one_total+=luxurious_lamp_price
-customer_one_itemization+="\n\n"+luxurious_lamp_description
-customer_one_tax=customer_one_total*sales_tax
-customer_one_total+=customer_one_tax
-print("Customer One Items:")
-print(customer_one_itemization + "\n")
-print("Customer One Total:")
-print("$",customer_one_total)
+print("Here are the planets avalible:\n")
+print("1. Venus   2. Mars    3. Jupiter")
+print("4. Saturn  5. Uranus  6. Neptune\n")
+
+# Asking the user for needed information
+weight = int(input("What is your weight? "))
+planet = int(input("Please choose a planet number: "))
+
+# Initializing the planet relative gravity variables
+venus,mars,jupiter,saturn,uranus,neptune=0.91,0.38,2.34,1.06,0.92,1.19
+
+# Checking which planet the user selcted and giving them the correct weight output
+if planet==1:
+  print(weight*venus)
+if planet==2:
+  print(weight*mars)
+if planet==3:
+  print(weight*jupiter)
+if planet==4:
+  print(weight*saturn)
+if planet==5:
+  print(weight*uranus)
+if planet==6:
+  print(weight*neptune)
