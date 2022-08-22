5b7a3f39b8b14840f646cc833d08e211228a9508
PronetAI <pronetaicorp@gmail.com>
2022-08-21 17:57:15 -0700

    Create corrected_area_calculator_program.py

---
+++ Errors in Python/corrected_area_calculator_program.py
@@ -0,0 +1,14 @@
+# Corrected Area Calculator 📏
+import math
+
+base, height = 20, 30
+area= base * height / 2
+print("The triangle area is", area)
+
+length, width = 2, 12
+area = length * width
+print("The rectangle area is" + str(area))
+    
+radius = 36
+area = math.pi * radius * radius
+print("The circle area is", area)
