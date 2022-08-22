586007c0d8692f79c56da258987eebbfaa3e7a43
PronetAI <pronetaicorp@gmail.com>
2022-08-21 17:53:48 -0700

    Create corrected_who_wants_to_be_a_millionare_program.py

---
+++ Errors in Python/corrected_who_wants_to_be_a_millionare_program.py
@@ -0,0 +1,17 @@
+# Corrected Who Wants To Be A Millionaire 💰 
+
+score = 0
+
+option1,option2,option3,option4 = 'Fresca','V8','Honda','A&W
+print("For ordering his favorite beverages on demand, LBJ had four buttons installed in the Oval Office labeled 'Coffee', 'Tea', 'Coke', and what?\n")
+print("A.", option1)
+print("B.", option2)
+print("C.", option3)
+print("D.", option4)
+
+answer = 'a'
+if answer == 'A' or answer == 'a': 
+  score += 100
+  print("\nCorrect!")
+else:
+  print("\nNope, sorry!")
